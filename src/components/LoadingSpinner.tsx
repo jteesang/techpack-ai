4544d770341e8f109f8f2e96f5d6846802bec2db
Julia Tseng <juliatseng12345@gmail.com>
2024-09-22 19:02:18 -0700

    Remove some files

--- src/components/LoadingSpinner.tsx
+++
@@ -1,5 +0,0 @@
-const LoadingSpinner = () => (
-  <div className="w-12 h-12 border-4 border-t-4 border-gray-200 border-solid rounded-full animate-spin"></div>
-  );
-
-export default LoadingSpinner;
