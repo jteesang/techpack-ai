854f2efc59d5c02caca28d2bc4e0ba7d72cefe3a
Julia Tseng <juliatseng12345@gmail.com>
2024-10-15 15:56:43 -0700

    Remove unused file

--- src/app/services/hello.ts
+++
@@ -1,9 +0,0 @@
-export async function handler(req: Request) {
-    const { name } = new URL(req.url).searchParams;
-    return new Response(JSON.stringify({ message: `Hello, ${name || 'World'}!` }), {
-        status: 200,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-}
