d5346bf185ec2dfd8b8e289db340455130846c83
Julia Tseng <juliatseng12345@gmail.com>
2024-12-12 16:14:33 -0800

    Add page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import TechpackLanding from '@/components/LandingPage';
+import { UserProvider } from '@/context/UserContext';
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <UserProvider>
+        <TechpackLanding />
+      </UserProvider>
+    </div>
+  );
+};
+
+export default Home;
